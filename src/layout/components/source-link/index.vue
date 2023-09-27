9981a51996ca9d7ddcf9ef32d3e584524b5b4d6a
xiaojunnuo <xiaojunnuo@qq.com>
2023-09-27 11:41:41 +0800

    chore:

--- src/layout/components/source-link/index.vue
+++ src/layout/components/source-link/index.vue
@@ -22,7 +22,7 @@ export default defineComponent({
       },
       { immediate: true }
     );
-    const middle = "/fast-crud/fs-admin-antdv/tree/main/src/views";
+    const middle = "/fast-crud/fs-admin-antdv4/tree/main/src/views";
     function goSource(prefix: any) {
       const path = router.currentRoute.value.fullPath;
       window.open(prefix + middle + path + "/index.vue");
